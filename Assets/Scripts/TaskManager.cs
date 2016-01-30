8caa94992bc5095e806eb09f314ee4975a47a26e
Tuisku Haro <tuisku.haro@gmail.com>
2016-01-30 22:21:38 +0200

    initial progress and expiriation bars

--- Assets/Scripts/TaskManager.cs
+++ Assets/Scripts/TaskManager.cs
@@ -3,17 +3,22 @@ using System.Collections;
 
 public class TaskManager : MonoBehaviour
 {
-	public float LifeTime = 3.0f;
+	public float LifeTime = 10.0f;
 	public string keyStrokes;
 	public int keyNr = 0;
 	public int repetitionNr = 0;
 	public int totalReps;
+	
+	public GameObject expiriationBar;
 	public GameObject progressBarTemplate;
 	private GameObject progressBarGO;
+	private GameObject expiriationBarGO;
+	private Transform expiriationT;
 	private Transform progressT;
 	private float creationTime;
 	private int gridPosition = -1;
-
+	private float expBarMultiplier =-0.1666f;
+	
 	// Use this for initialization
 	void Start ()
 	{
@@ -21,7 +26,13 @@ public class TaskManager : MonoBehaviour
 		GameObject progressBarGO = GameObject.Instantiate(progressBarTemplate) as GameObject;
 		progressT = progressBarGO.transform;
 		progressBarGO.transform.SetParent(gameObject.transform, false);
-		progressT.localPosition = new Vector3(0.0f, 1.0f, -6.0f);
+		progressT.localPosition = new Vector3(-1.0f, -0.85f, -6.0f);
+		
+		GameObject expiriationBarGO = GameObject.Instantiate (expiriationBar) as GameObject;
+		expiriationT = expiriationBarGO.transform;
+		expiriationBarGO.transform.SetParent(gameObject.transform, false);
+		expiriationT.localPosition = new Vector3(-1.0f, 1.85f, -6.0f);
+		
 	}
 	
 	public void SetGridPosition (int pos)
@@ -40,8 +51,9 @@ public class TaskManager : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		progressT.localScale = new Vector3( (float)(keyNr + (repetitionNr * keyStrokes.Length)) / (float) (totalReps* keyStrokes.Length), 1.0f, 1.0f);
-
+		progressT.localScale = new Vector3( (float)(keyNr + (repetitionNr * keyStrokes.Length)) / (float) (totalReps* keyStrokes.Length) * (float)1.5, 1.5f, 1.0f);
+		expiriationT.localScale = new Vector3 (1.5f - (creationTime - Time.time) * (LifeTime * expBarMultiplier), 1.0f, 1.0f);
+		
 		if (keyNr == keyStrokes.Length) {
 			repetitionNr++;
 			keyNr = 0;
@@ -65,11 +77,11 @@ public class TaskManager : MonoBehaviour
 				keyNr++;
 			}*/
 		}
-		/*
+		
 		if (Time.time > creationTime + LifeTime)
 		{
 			TaskCompleted();
 		}
-	*/
+		
 	}
 }
