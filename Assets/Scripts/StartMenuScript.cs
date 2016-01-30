1d2bb21d37c2b150b27029699ca0d90faa7b9842
Sulka Haro <sulka@sulka.net>
2016-01-30 21:23:13 +0200

    Star menu added

---
+++ Assets/Scripts/StartMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartMenuScript : MonoBehaviour {
+
+	public Button startButton;
+	public Button exitButton;
+
+	// Use this for initialization
+	void Start () {
+		startButton = startButton.GetComponent <Button> ();
+		exitButton = exitButton.GetComponent <Button> ();
+	}
+	
+	public void ExitPress() 
+	{
+		Application.Quit ();
+	}
+
+	public void StartGame()
+	{
+		Application.LoadLevel ("Main");
+
+	}
+
+}
